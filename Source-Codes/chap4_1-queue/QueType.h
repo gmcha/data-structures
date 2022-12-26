36f2ff20e55878151661492ea2b368aab38596e3
Ga Min Cha <prettygamin@naver.com>
2022-12-26 23:43:09 +0900

    Create: QueType.h

---
+++ Source-Codes/chap4_1-queue/QueType.h
@@ -0,0 +1,17 @@
+// dynamic array  implementation
+template <class ItemType>
+
+class QueType{
+    public:
+        QueType();
+        QueType(int max);
+        ~QueType();
+        bool IsFull() const;
+        void Enqueue(ItemType item);
+        void Dequeue(ItemType& item);
+    private:
+        int front;
+        int rear;
+        int maxQue;
+        ItemType* items; 
+};
